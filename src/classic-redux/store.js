eb38d2baa3cbe78869d69c76f40d2de880b78ecf
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-05-13 21:06:27 +0400

    Добавили redux-saga

--- src/classic-redux/store.js
+++ src/classic-redux/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
-import { thunk } from "redux-thunk";
+import rootSaga from "../redux-saga/rootSaga";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
--- src/classic-redux/users/thunkActions.js
+++ src/classic-redux/users/thunkActions.js
@@ -4,16 +4,24 @@ import {
   fetchUserSuccess,
 } from "./actionsCreators";
 
+// export const fetchUserData = (userId) => {
+//   console.log("thunkActions.js: fetchUserData", userId);
+//   return (dispatch) => {
+//     dispatch(fetchUserRequest());
+//     fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         dispatch(fetchUserSuccess(data));
+//       })
+//       .catch((error) => {
+//         dispatch(fetchUserFailure(error.message));
+//       });
+//   };
+// };
+
 export const fetchUserData = (userId) => {
-  return (dispatch) => {
-    dispatch(fetchUserRequest());
-    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch(fetchUserSuccess(data));
-      })
-      .catch((error) => {
-        dispatch(fetchUserFailure(error.message));
-      });
+  return {
+    type: "FETCH_USER_REQUEST",
+    payload: userId,
   };
 };
